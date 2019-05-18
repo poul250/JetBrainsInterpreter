e899d30c97d5b8773868731f5dbf0cccf928aee0
Kulis Pavel <poul250@yandex.ru>
2019-05-18 20:26:01 +0300

    modified Context class for functions support

--- src/com/pawka/interpreter/Context.java
+++ src/com/pawka/interpreter/Context.java
@@ -1,18 +1,33 @@
 package com.pawka.interpreter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Context {
     public Stack<Integer> stack;
+    public ArrayList<String> argsList;
+    public HashMap<String, Integer> variables;
+    public static HashMap<String, Program> functions = new HashMap<>();
     public int address;
 
     public Context() {
+        this(new ArrayList<>());
+    }
+
+    public Context(ArrayList<String> argsList) {
+        this.argsList = argsList;
         stack = new Stack<>();
+        variables = new HashMap<>();
         refresh();
     }
 
     public void refresh() {
         address = 0;
         stack.clear();
+    }
+
+    public static void addFunction(String name, Program program) {
+        functions.put(name, program);
     }
 }
