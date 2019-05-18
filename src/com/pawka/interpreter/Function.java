1e4a578a3d98f8f08fd4d5085299c218da90c5c7
Kulis Pavel <poul250@yandex.ru>
2019-05-18 22:10:20 +0300

    Add constructor to Function

--- src/com/pawka/interpreter/Function.java
+++ src/com/pawka/interpreter/Function.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class Function {
     public Commands commands;
     public ArrayList<String> params;
+
+    public Function (ArrayList<String> params, Commands commands) {
+        this.params = params;
+        this.commands = commands;
+    }
 }
